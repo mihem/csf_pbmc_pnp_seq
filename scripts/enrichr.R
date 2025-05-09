6275ed5fc7a4a58cedbd552a78a485e20d77e005
mischko <mhem@mailbox.org>
2025-05-09 21:03:31 +0200

    refactore enrichr

--- scripts/enrichr.R
+++ scripts/enrichr.R
@@ -1,5 +1,5 @@
 ###############################################################################
-# perform gene set enrichment annalysis of CD8_NK population
+# perform gene set enrichment analysis of CD8_NK population
 # requires running annotate.R first
 ###############################################################################
 
@@ -11,12 +11,12 @@ library(tidyverse)
 library(patchwork)
 library(readxl)
 library(writexl)
+library(rlang)
 
 # load preprocessed data ----
 sc_merge <- qs::qread(file.path("objects", "sc_merge.qs"), nthreads = 6)
 
 # get enrichment dbs ----
-dbs <- enrichR::listEnrichrDbs()
 dbs <- c(
     "TF_Perturbations_Followed_by_Expression",
     "Transcription_Factor_PPIs",
@@ -29,90 +29,54 @@ dbs <- c(
     "GO_Molecular_Function_2023"
 )
 
-# get top markers ----
-cluster_sel <- c("CD8_NK")
-top <-
-    lapply(c(cluster_sel), FUN = function(x) {
-        read_excel(file.path("results", "de", "topmarkers.xlsx"), sheet = x) |>
-            dplyr::filter(
-                p_val_adj < 0.05
-            ) |>
-            dplyr::filter(
-                avg_log2FC > 2
-            ) |>
-            dplyr::slice_max(
-                order_by = avg_log2FC,
-                n = 100,
-                with_ties = FALSE
-            )
-    }) |>
-    setNames(c(cluster_sel))
-
-# get de markers ----
-de_cidp_ctrl_csf_clusters <- c("CD8_NK", "CD4TCM_2")
-
-de_cidp_ctrl_csf_pos <-
-    lapply(c(de_cidp_ctrl_csf_clusters), FUN = function(x) {
-        readxl::read_excel(
-            file.path("results", "de", "de_cidp_ctrl_csf.xlsx"),
-            sheet = x
+# Helper functions ----
+get_top_genes <- function(
+    file_path,
+    sheet_name,
+    n = 100,
+    min_log2fc = 2,
+    max_padj = 0.05
+) {
+    read_excel(file_path, sheet = sheet_name) |>
+        dplyr::filter(
+            p_val_adj < max_padj,
+            avg_log2FC > min_log2fc
         ) |>
-            dplyr::filter(
-                p_val_adj < 0.05
-            ) |>
-            dplyr::filter(
-                avg_log2FC > 2
-            ) |>
-            dplyr::slice_max(
-                order_by = avg_log2FC,
-                n = 100,
-                with_ties = FALSE
-            )
-    }) |>
-    setNames(c(de_cidp_ctrl_csf_clusters))
-
-# perform enrichment with selected clusters of topmarkers ----
-top_enrichr <-
-    lapply(c(cluster_sel), FUN = function(x) {
-        enrichr(top[[x]]$gene, dbs)
-    }) |>
-    setNames(c(cluster_sel))
-
-lapply(
-    c(cluster_sel),
-    FUN = function(x) {
-        write_xlsx(
-            top_enrichr[[x]],
-            file.path("results", "enrichr", paste0("enrichr_", x, ".xlsx"))
+        dplyr::slice_max(
+            order_by = avg_log2FC,
+            n = n,
+            with_ties = FALSE
         )
-    }
-)
+}
 
-# perform enrichment with selected clusters of de_cidp_ctrl_csf ----
-de_cidp_ctrl_csf_enrichr_pos <-
-    lapply(c(de_cidp_ctrl_csf_clusters), FUN = function(x) {
-        enrichr(de_cidp_ctrl_csf_pos[[x]]$gene, dbs)
-    }) |>
-    setNames(c(de_cidp_ctrl_csf_clusters))
+perform_enrichment <- function(clusters, file_path, prefix = "") {
+    # Get enrichment results
+    enrichr_results <- lapply(clusters, function(x) {
+        genes <- get_top_genes(file_path, x)$gene
+        enrichr_res <- enrichr(genes, dbs)
 
-lapply(
-    de_cidp_ctrl_csf_clusters,
-    FUN = function(x) {
+        # Write results to Excel
         write_xlsx(
-            de_cidp_ctrl_csf_enrichr_pos[[x]],
+            enrichr_res,
             file.path(
                 "results",
                 "enrichr",
-                paste0("enrichr_de_cidp_ctrl_csf_pos_", x, ".xlsx")
+                paste0("enrichr_", prefix, x, ".xlsx")
             )
         )
-    }
-)
 
-# function to plot enrichment results ----
+        enrichr_res
+    }) |>
+        setNames(clusters)
+
+    # Plot results
+    plot_enrichment(clusters, prefix)
+
+    return(enrichr_results)
+}
+
 plotEnrichrFun <- function(filename, sheet, width, height) {
-    colors <- RColorBrewer::brewer.pal(5, "Set2")
-    enrichr <- readxl::read_excel(
+    plot_data <- readxl::read_excel(
         file.path("results", "enrichr", glue::glue("enrichr_{filename}.xlsx")),
         sheet = sheet
     ) |>
@@ -121,60 +85,71 @@ plotEnrichrFun <- function(filename, sheet, width, height) {
             n = 10,
             with_ties = FALSE
         ) |>
-        tidyr::separate(Overlap, into = c("overlap1", "overlap2")) |> # separate overlap in two columns
+        tidyr::separate(Overlap, into = c("overlap1", "overlap2")) |>
         dplyr::mutate(
-            Term = gsub(x = Term, pattern = "\\s\\(.+\\)", replacement = "")
-        ) |>
-        dplyr::mutate(overlap = as.numeric(overlap1) / as.numeric(overlap2)) |> # calculcate overlap
-        ggplot(aes(
+            Term = gsub(
+                x =  Term,
+                pattern = "\\s\\(.+\\)",
+                replacement = ""
+            ),
+            overlap = as.numeric(overlap1) / as.numeric(overlap2)
+        )
+
+    p <- ggplot2::ggplot(
+        plot_data,
+        ggplot2::aes(
             y = reorder(Term, -log10(Adjusted.P.value)),
             x = -log10(Adjusted.P.value)
-        )) +
-        geom_col(fill = scales::hue_pal()(5)[1]) +
-        labs(
+        )
+    ) +
+        ggplot2::geom_col(fill = scales::hue_pal()(5)[1]) +
+        ggplot2::labs(
             x = "-Log10 Adjusted P value",
             y = ""
         ) +
-        theme_classic() +
-        theme(legend.position = "none")
-    ggsave(
+        ggplot2::theme_classic() +
+        ggplot2::theme(legend.position = "none")
+
+    ggplot2::ggsave(
         file.path(
             "results",
             "enrichr",
             glue::glue("barplot_enrichr_{filename}_{sheet}.pdf")
         ),
+        plot = p,
         width = width,
         height = height
     )
 }
 
-# plot enrichment of topmarkers ----
-lapply(
-    c(cluster_sel),
-    FUN = function(x) {
-        plotEnrichrFun(
-            x,
-            sheet = "GO_Biological_Process_2023",
-            width = 6,
-            height = 2
-        )
-    }
+plot_enrichment <- function(
+    clusters,
+    prefix = "",
+    sheet = "GO_Biological_Process_2023",
+    width = 6,
+    height = 2
+) {
+    filenames <- if (prefix == "") clusters else paste0(prefix, clusters)
+    lapply(filenames, function(x) {
+        plotEnrichrFun(x, sheet = sheet, width = width, height = height)
+    })
+}
+
+# Main analysis ----
+# Analysis parameters
+cluster_sel <- c("CD8_NK")
+de_cidp_ctrl_csf_clusters <- c("CD8_NK", "CD4TCM_2")
+
+# Perform enrichment analysis for top markers
+top_enrichr <- perform_enrichment(
+    clusters = cluster_sel,
+    file_path = file.path("results", "de", "topmarkers.xlsx"),
+    prefix = "topmarkers_"
 )
 
-# plot enrichment of de_cidp_ctrl_csf ----
-enrichr_filenames_de_cidp_csf_pos <- c(
-    "de_cidp_ctrl_csf_pos_CD8_NK",
-    "de_cidp_ctrl_csf_pos_CD4TCM_2"
-)
-
-lapply(
-    c(enrichr_filenames_de_cidp_csf_pos),
-    FUN = function(x) {
-        plotEnrichrFun(
-            x,
-            sheet = "GO_Biological_Process_2023",
-            width = 6,
-            height = 2
-        )
-    }
+# Perform enrichment analysis for DE genes
+de_cidp_ctrl_csf_enrichr_pos <- perform_enrichment(
+    clusters = de_cidp_ctrl_csf_clusters,
+    file_path = file.path("results", "de", "de_cidp_ctrl_csf.xlsx"),
+    prefix = "de_cidp_ctrl_csf_pos_"
 )

adfecd14c5930c6fc8382532697e47a20055c7a4
mischko <mhem@mailbox.org>
2025-07-29 20:43:13 +0200

    add subgroup analysis

--- scripts/abundance.R
+++ scripts/abundance.R
@@ -17,7 +17,7 @@ library(speckle)
 sc_merge <- qs::qread(file.path("objects", "sc_merge.qs"), nthread = 4)
 
 sc_merge_csf <- subset(sc_merge, tissue %in% "CSF")
-sc_merge_pbmc <- subset(sc_merge, tissue %in% "PBMC")
+sc_merge_csf <- subset(sc_merge, tissue %in% "PBMC")
 
 # meta data ----
 lookup <- qs::qread(file.path("objects", "lookup.qs"))
@@ -39,12 +39,12 @@ scMisc::stackedPlot(
 )
 
 scMisc::stackedPlot(
-  object = sc_merge_pbmc,
+  object = sc_merge_csf,
   x_axis = "sample",
   y_axis = "cluster",
-  x_order = unique(sc_merge_pbmc$sample),
-  y_order = sc_merge_pbmc@misc$cluster_order,
-  color = sc_merge_pbmc@misc$cluster_col,
+  x_order = unique(sc_merge_csf$sample),
+  y_order = sc_merge_csf@misc$cluster_order,
+  color = sc_merge_csf@misc$cluster_col,
   width = 7
 )
 
@@ -59,12 +59,12 @@ scMisc::stackedPlot(
 )
 
 scMisc::stackedPlot(
-  object = sc_merge_pbmc,
+  object = sc_merge_csf,
   x_axis = "group",
   y_axis = "cluster",
-  x_order = unique(sc_merge_pbmc$group),
-  y_order = sc_merge_pbmc@misc$cluster_order,
-  color = sc_merge_pbmc@misc$cluster_col,
+  x_order = unique(sc_merge_csf$group),
+  y_order = sc_merge_csf@misc$cluster_order,
+  color = sc_merge_csf@misc$cluster_col,
   width = 3.8
 )
 
@@ -79,12 +79,12 @@ scMisc::stackedPlot(
 )
 
 scMisc::stackedPlot(
-  object = sc_merge_pbmc,
+  object = sc_merge_csf,
   x_axis = "diagnosis",
   y_axis = "cluster",
-  x_order = unique(sc_merge_pbmc$diagnosis),
-  y_order = sc_merge_pbmc@misc$cluster_order,
-  color = sc_merge_pbmc@misc$cluster_col,
+  x_order = unique(sc_merge_csf$diagnosis),
+  y_order = sc_merge_csf@misc$cluster_order,
+  color = sc_merge_csf@misc$cluster_col,
   width = 5
 )
 
@@ -132,8 +132,10 @@ propeller_results <-
 
 # Create filenames
 filenames <- paste0(
-  combinations$tissue, "_", 
-  combinations$condition1, "_", 
+  combinations$tissue,
+  "_",
+  combinations$condition1,
+  "_",
   combinations$condition2
 )
 
@@ -165,13 +167,83 @@ scMisc::abBoxPlot(
 )
 
 scMisc::abBoxPlot(
-  object = sc_merge_pbmc,
+  object = sc_merge_csf,
   cluster_idents = "cluster",
   sample = "sample",
-  cluster_order = sc_merge_pbmc@misc$cluster_order,
+  cluster_order = sc_merge_csf@misc$cluster_order,
   group_by = "diagnosis",
-  group_order = sc_merge_pbmc@misc$diagnosis_order,
-  color = sc_merge_pbmc@misc$diagnosis_col,
+  group_order = sc_merge_csf@misc$diagnosis_order,
+  color = sc_merge_csf@misc$diagnosis_col,
   number_of_tests = choose(9, 2),
   width = 14
+)
+
+# investigate subgroup of CIDP/GBS which has NKCD56bright_1 cells
+sc_merge_csf_subgroup <- sc_merge_csf
+
+nkbright_high_group <- c("P16", "P17", "P18", "P21", "P23")
+sc_merge_csf_subgroup$subgroup <- ifelse(
+  sc_merge_csf_subgroup$patient %in% nkbright_high_group,
+  "NKhi",
+  as.character(sc_merge_csf_subgroup$diagnosis)
+)
+
+sc_merge_csf_subgroup@misc$diagnosis_order <- c(
+  "CTRL",
+  "NKhi",
+  "CIAP",
+  "GBS",
+  "CIDP",
+  "MAG",
+  "MFS",
+  "PNC",
+  "CAN",
+  "PPN"
+)
+
+scMisc::abBoxPlot(
+  object = sc_merge_csf_subgroup,
+  cluster_idents = "cluster",
+  sample = "sample",
+  cluster_order = sc_merge_csf_subgroup@misc$cluster_order,
+  group_by = "subgroup",
+  group_order = sc_merge_csf_subgroup@misc$diagnosis_order,
+  color = sc_merge_csf_subgroup@misc$diagnosis_col,
+  number_of_tests = choose(9, 2),
+  width = 16
+)
+
+lookup_subgroup <- lookup |>
+  dplyr::mutate(
+    subgroup = dplyr::case_when(
+      patient %in% nkbright_high_group ~ "NKhi",
+      diagnosis %in% c("CIDP", "GBS") ~ "IN",
+      TRUE ~ as.character(diagnosis)
+    )
+  )
+
+sc_merge_csf_subgroup$subgroup <- dplyr::case_when(
+  sc_merge_csf_subgroup$patient %in% nkbright_high_group ~ "NKhi",
+  sc_merge_csf_subgroup$diagnosis %in% c("CIDP", "GBS") ~ "IN",
+  TRUE ~ as.character(sc_merge_csf_subgroup$diagnosis)
+)
+
+propeller_results_subgroup_csf <-
+  scMisc::propellerCalc(
+    seu_obj1 = sc_merge_csf_subgroup,
+    condition1 = "NKhi",
+    condition2 = "IN",
+    cluster_col = "cluster",
+    meta_col = "subgroup",
+    lookup = lookup_subgroup,
+    sample_col = "patient",
+    formula = "~0 + subgroup + sex + age",
+    min_cells = 9
+  )
+
+scMisc::plotPropeller(
+  data = propeller_results_subgroup_csf,
+  color = sc_merge@misc$cluster_col,
+  filename = "CSF_NKhi_IN",
+  FDR = 0.1
 )

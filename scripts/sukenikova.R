ab33ef2a03203667c0a78848e8a8b42cb7cc07cc
mischko <mhem@mailbox.org>
2025-09-14 20:15:33 +0200

    improve sukenikova analysis

--- scripts/sukenikova.R
+++ scripts/sukenikova.R
@@ -4,7 +4,8 @@
 ##################################################
 library(tidyverse)
 library(qs)
-
+library(ggplot2)
+library(ggalluvial)
 
 # Sukenikova supp table 2
 # PNS-myelin-reactivi clones
@@ -15,42 +16,14 @@ sukenikova_reactive_clones <-
     dplyr::rename(CTAA = cdr3b_aa) |>
     dplyr::rename_with(function(x) paste0("sukenikova_", x), .cols = -CTAA)
 
-# Sukenikova public files
-sukenikova_files <- list.files(
-    file.path("raw", "sukenikova"),
-    pattern = ".tsv",
-    # pattern = "AC",
-    full.names = TRUE
-)
-
-# read in the files and combine into single data frame
-sukenikova_data <- lapply(latorre_files, readr::read_tsv)
-sukenikova_combined <- bind_rows(latorre_data) |>
-    dplyr::select(sample_name, amino_acid) |>
-    dplyr::rename(
-        CTaa = amino_acid
-    ) |>
-    drop_na() |>
-    dplyr::distinct()
-
-# sanity check
-sukenikova_combined |>
-    group_by(sample_name, CTaa) |>
-    filter(n() > 1)
-
-qsave(
-    sukenikova_combined,
-    file = file.path("objects", "sukenikova_combined.qs")
-)
-
 # read this dataset
 sc_tcr <- qread(file.path("objects", "sc_tcr.qs"))
 
 # extract CTaa from TRB data from sc_tcr object
 heming_tcr <- sc_tcr@meta.data |>
-    dplyr::select(sample, CTaa, tissue_diagnosis) |>
+    dplyr::select(sample, CTaa, tissue, diagnosis) |>
     tidyr::separate_wider_delim(CTaa, names = c("TRA", "TRB"), delim = "_") |>
-    dplyr::select(sample, TRB, tissue_diagnosis) |>
+    dplyr::select(sample, TRB, tissue, diagnosis) |>
     tidyr::drop_na() |>
     dplyr::distinct() |>
     dplyr::rename(CTaa = TRB) |>
@@ -74,48 +47,102 @@ writexl::write_xlsx(
     path = file.path("results", "tcr", "sukenikova_reactive_shared.xlsx")
 )
 
-# compare with sukenikova public data
-shared_clones_sukenikova <- heming_tcr |>
-    dplyr::inner_join(sukenikova_combined, by = c("CTaa"))
-
-# sanity check
-heming_tcr |>
-    dplyr::filter(CTaa == "CASSRTGPYGYTF")
-
-sukenikova_combined |>
-    dplyr::filter(CTaa == "CASSRTGPYGYTF")
-
-shared_clones_sukenikova |>
-    dplyr::group_by(tissue_diagnosis, sample_name) |>
-    dplyr::summarise(n = n()) |>
-    dplyr::filter(tissue_diagnosis == "CSF_CTRL") |>
-    print(n = Inf)
-
-# frequency of tissue_diagnosis in shared clones
-table(shared_clones_sukenikova$tissue_diagnosis)
-
-plot_shared_clones_sukenikova <-
-    as.data.frame(
-        table(shared_clones_sukenikova$tissue_diagnosis) /
-            table(heming_tcr$tissue_diagnosis)
-    ) |>
+# alluvial plot
+sukenikova_reactive_plot <-
+    sukenikova_reactive_shared |>
     ggplot(aes(
-        x = Var1,
-        y = Freq,
-        fill = Var1
+        axis1 = CTaa,
+        axis2 = heming_tissue,
+        axis3 = heming_diagnosis,
+        axis4 = sukenikova_specificity
     )) +
-    geom_col() +
-    scale_fill_manual(values = sc_tcr@misc$tissue_diagnosis_col) +
-    theme_classic() +
-    theme(
-        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
-        legend.position = "none"
+    scale_x_discrete(
+        limits = c("CTaa", "Tissue",  "Diagnosis", "Specificity"),
+        expand = c(.1, .05)
     ) +
-    xlab("")
+    geom_alluvium(aes(fill = sukenikova_specificity)) +
+    geom_stratum() +
+    geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+    theme_minimal() +
+    ylab("Frequency")
 
 ggsave(
-    plot = plot_shared_clones_sukenikova,
-    filename = file.path("results", "tcr", "shared_clones_sukenikova.pdf"),
-    width = 6,
-    height = 4
+    plot = sukenikova_reactive_plot,
+    filename = file.path("results", "tcr", "sukenikova_reactive_alluvial.pdf"),
+    width = 8,
+    height = 6
 )
+
+# # Sukenikova public files
+# sukenikova_files <- list.files(
+#     file.path("raw", "sukenikova"),
+#     pattern = ".tsv",
+#     # pattern = "AC",
+#     full.names = TRUE
+# )
+
+# # read in the files and combine into single data frame
+# sukenikova_data <- lapply(sukenikova_files, readr::read_tsv)
+# sukenikova_combined <- bind_rows(sukenikova_data) |>
+#     dplyr::select(sample_name, amino_acid) |>
+#     dplyr::rename(
+#         CTaa = amino_acid
+#     ) |>
+#     drop_na() |>
+#     dplyr::distinct()
+
+# # sanity check
+# sukenikova_combined |>
+#     group_by(sample_name, CTaa) |>
+#     filter(n() > 1)
+
+# qsave(
+#     sukenikova_combined,
+#     file = file.path("objects", "sukenikova_combined.qs")
+# )
+
+# # compare with sukenikova public data
+# shared_clones_sukenikova <- heming_tcr |>
+#     dplyr::inner_join(sukenikova_combined, by = c("CTaa"))
+
+# # sanity check
+# heming_tcr |>
+#     dplyr::filter(CTaa == "CASSRTGPYGYTF")
+
+# sukenikova_combined |>
+#     dplyr::filter(CTaa == "CASSRTGPYGYTF")
+
+# shared_clones_sukenikova |>
+#     dplyr::group_by(tissue_diagnosis, sample_name) |>
+#     dplyr::summarise(n = n()) |>
+#     dplyr::filter(tissue_diagnosis == "CSF_CTRL") |>
+#     print(n = Inf)
+
+# # frequency of tissue_diagnosis in shared clones
+# table(shared_clones_sukenikova$tissue_diagnosis)
+
+# plot_shared_clones_sukenikova <-
+#     as.data.frame(
+#         table(shared_clones_sukenikova$tissue_diagnosis) /
+#             table(heming_tcr$tissue_diagnosis)
+#     ) |>
+#     ggplot(aes(
+#         x = Var1,
+#         y = Freq,
+#         fill = Var1
+#     )) +
+#     geom_col() +
+#     scale_fill_manual(values = sc_tcr@misc$tissue_diagnosis_col) +
+#     theme_classic() +
+#     theme(
+#         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+#         legend.position = "none"
+#     ) +
+#     xlab("")
+
+# ggsave(
+#     plot = plot_shared_clones_sukenikova,
+#     filename = file.path("results", "tcr", "shared_clones_sukenikova.pdf"),
+#     width = 6,
+#     height = 4
+# )

fa87a012db24c3973ff937d7621497f1f88fe1ea
mischko <mhem@mailbox.org>
2025-09-14 19:44:40 +0200

    change name to first author

--- scripts/latorre.R
+++ scripts/latorre.R
@@ -1,31 +1,31 @@
 ##################################################
-# compare data with Latorre Nature 2024
+# compare data with sukenikova Nature 2024
 # https://www.nature.com/articles/s41586-023-06916-6
 ##################################################
 library(tidyverse)
 library(qs)
 
 
-# latorre supp table 2
+# sukenikova supp table 2
 # PNS-myelin-reactivi clones
-latorre_reactive_clones <-
+sukenikova_reactive_clones <-
     readr::read_csv(
-        file.path("lookup", "latorre_nature_gbs_supp_table_2.csv")
+        file.path("lookup", "sukenikova_nature_gbs_supp_table_2.csv")
     ) |>
     dplyr::rename(CTAA = cdr3b_aa) |>
-    dplyr::rename_with(function(x) paste0("latorre_", x), .cols = -CTAA)
+    dplyr::rename_with(function(x) paste0("sukenikova_", x), .cols = -CTAA)
 
-# latorre public files
-latorre_files <- list.files(
-    file.path("raw", "latorre"),
+# sukenikova public files
+sukenikova_files <- list.files(
+    file.path("raw", "sukenikova"),
     pattern = ".tsv",
     # pattern = "AC",
     full.names = TRUE
 )
 
 # read in the files and combine into single data frame
-latorre_data <- lapply(latorre_files, readr::read_tsv)
-latorre_combined <- bind_rows(latorre_data) |>
+sukenikova_data <- lapply(latorre_files, readr::read_tsv)
+sukenikova_combined <- bind_rows(latorre_data) |>
     dplyr::select(sample_name, amino_acid) |>
     dplyr::rename(
         CTaa = amino_acid
@@ -34,13 +34,13 @@ latorre_combined <- bind_rows(latorre_data) |>
     dplyr::distinct()
 
 # sanity check
-latorre_combined |>
+sukenikova_combined |>
     group_by(sample_name, CTaa) |>
     filter(n() > 1)
 
 qsave(
-    latorre_combined,
-    file = file.path("objects", "latorre_combined.qs")
+    sukenikova_combined,
+    file = file.path("objects", "sukenikova_combined.qs")
 )
 
 # read this dataset
@@ -57,46 +57,46 @@ heming_tcr <- sc_tcr@meta.data |>
     dplyr::rename_with(function(x) paste0("heming_", x), .cols = -CTaa)
 
 # compare with latore supp table 2
-latorre_reactive_shared <- heming_tcr |>
+sukenikova_reactive_shared <- heming_tcr |>
     dplyr::inner_join(
-        latorre_reactive_clones,
+        sukenikova_reactive_clones,
         by = c("CTaa" = "CTAA")
     ) |>
     relocate(CTaa, .before = everything())
 
 qsave(
-    latorre_reactive_shared,
-    file = file.path("objects", "latorre_reactive_shared.qs")
+    sukenikova_reactive_shared,
+    file = file.path("objects", "sukenikova_reactive_shared.qs")
 )
 
 writexl::write_xlsx(
-    latorre_reactive_shared,
-    path = file.path("results", "tcr", "latorre_reactive_shared.xlsx")
+    sukenikova_reactive_shared,
+    path = file.path("results", "tcr", "sukenikova_reactive_shared.xlsx")
 )
 
-# compare with Latorre public data
-shared_clones_latorre <- heming_tcr |>
-    dplyr::inner_join(latorre_combined, by = c("CTaa"))
+# compare with sukenikova public data
+shared_clones_sukenikova <- heming_tcr |>
+    dplyr::inner_join(sukenikova_combined, by = c("CTaa"))
 
 # sanity check
 heming_tcr |>
     dplyr::filter(CTaa == "CASSRTGPYGYTF")
 
-latorre_combined |>
+sukenikova_combined |>
     dplyr::filter(CTaa == "CASSRTGPYGYTF")
 
-shared_clones_latorre |>
+shared_clones_sukenikova |>
     dplyr::group_by(tissue_diagnosis, sample_name) |>
     dplyr::summarise(n = n()) |>
     dplyr::filter(tissue_diagnosis == "CSF_CTRL") |>
     print(n = Inf)
 
 # frequency of tissue_diagnosis in shared clones
-table(shared_clones_latorre$tissue_diagnosis)
+table(shared_clones_sukenikova$tissue_diagnosis)
 
-plot_shared_clones_latorre <-
+plot_shared_clones_sukenikova <-
     as.data.frame(
-        table(shared_clones_latorre$tissue_diagnosis) /
+        table(shared_clones_sukenikova$tissue_diagnosis) /
             table(heming_tcr$tissue_diagnosis)
     ) |>
     ggplot(aes(
@@ -114,8 +114,8 @@ plot_shared_clones_latorre <-
     xlab("")
 
 ggsave(
-    plot = plot_shared_clones_latorre,
-    filename = file.path("results", "tcr", "shared_clones_latorre.pdf"),
+    plot = plot_shared_clones_sukenikova,
+    filename = file.path("results", "tcr", "shared_clones_sukenikova.pdf"),
     width = 6,
     height = 4
 )

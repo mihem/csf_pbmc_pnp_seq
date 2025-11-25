882c25a18477ca43862ff9f096d01ecac45aecbc
mischko <mhem@mailbox.org>
2025-11-25 21:08:17 +0100

    create boxplots for nkt flow

--- scripts/nkt_flow.R
+++ scripts/nkt_flow.R
@@ -11,6 +11,10 @@ library(pals)
 # source functions ----
 source(file.path("scripts", "flow_helper.R"))
 
+# define and colors
+group2_colors <- setNames(pals::cols25(2), c("CTRL", "IN"))
+diagnosis_colors <- setNames(pals::cols25(3), c("CTRL", "GBS", "CIDP"))
+
 # load flow and lookup file ----
 # load flow data, keep only first measurement if multiple are available
 nkt_flow_pre <-
@@ -21,7 +25,9 @@ flow_metadata <-
   read_excel(file.path("lookup", "olink_flow_lookup.xlsx")) |>
   mutate(tissue = ifelse(grepl("C", SampleID), "PBMC", "CSF")) |>
   dplyr::filter(tissue == "PBMC") |>
-  mutate(sample = gsub("(\\d+)_(\\d).*", "\\1-V\\2", SampleID))
+  mutate(sample = gsub("(\\d+)_(\\d).*", "\\1-V\\2", SampleID)) |>
+  mutate(group2 = factor(group2, levels = c("CTRL", "IN"))) |>
+  mutate(diagnosis = factor(diagnosis, levels = c("CTRL", "GBS", "CIDP")))
 
 # sanity checks
 anyDuplicated(nkt_flow_pre$sample)
@@ -46,6 +52,27 @@ nrow(nkt_flow) == nrow(nkt_flow_pre)
 
 # Extract flow variables -----
 flow_vars <-
-  nkt_flow$CSF |>
-  select(Gran:intMono) |>
+  nkt_flow |>
+  select(NKT:NK) |>
   names()
+
+# Create all boxplots -----
+createAndSaveBoxplots(
+  data = nkt_flow,
+  flow_vars = flow_vars,
+  group = "group2",
+  cols = group2_colors,
+  output_file = "nkt_plots_group2.pdf",
+  width = 10,
+  height = 10
+)
+
+createAndSaveBoxplots(
+  data = nkt_flow,
+  flow_vars = flow_vars,
+  group = "diagnosis",
+  cols = diagnosis_colors,
+  output_file = "nkt_plots_diagnosis.pdf",
+  width = 10,
+  height = 10
+)

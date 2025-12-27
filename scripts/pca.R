3077c4e7334f112e0af504fb9d1c448899a4751d
mischko <mhem@mailbox.org>
2025-12-27 21:37:53 +0100

    add combined pca analysis

--- scripts/pca.R
+++ scripts/pca.R
@@ -1,7 +1,6 @@
 ##################################################
 # PCA plots of cluster abundance across samples
 # requires running annotate.R first
-# not part of final analysis
 ##################################################
 
 # libraries ---
@@ -28,37 +27,108 @@ sc_merge_pbmc_main <- subset(
   subset = diagnosis %in% c("CIDP", "GBS", "CIAP", "CTRL")
 )
 
-scMisc::pcaSeurat(
-  object = sc_merge_csf_main,
-  cluster = "cluster",
-  sample = "sample",
-  condition = "diagnosis",
-  width = 20,
-  height = 5,
-  dir_output = file.path("results", "abundance")
-)
-
 pcaSeurat1 <- function(
-  object,
+  object_list,
   cluster,
   sample,
+  patient,
   condition,
+  tissue = NULL,
+  object_name = NULL,
   width = 20,
   height = 5,
   dir_output = "."
 ) {
-  if (!inherits(object, "Seurat")) {
-    stop("Object must be a Seurat object")
+  # If single object, convert to list
+  if (inherits(object_list, "Seurat")) {
+    object_list <- list(object_list)
   }
-  object_parse <- deparse(substitute(object))
 
-  props <- speckle::getTransformedProps(
-    clusters = object@meta.data[[cluster]],
-    sample = object@meta.data[[sample]],
-    transform = "logit"
-  )
+  # Validate all objects are Seurat
+  if (!all(sapply(object_list, inherits, "Seurat"))) {
+    stop("All objects must be Seurat objects")
+  }
 
-  cl_size <- t(as.data.frame.matrix(props$TransformedProps))
+  # Auto-capture object name if not provided
+  if (is.null(object_name)) {
+    object_name <- deparse1(substitute(object_list))
+  }
+
+  # Calculate proportions separately for each object
+  if (length(object_list) == 1) {
+    # Single tissue: patients as rows, clusters as columns
+    obj <- object_list[[1]]
+    props <- speckle::getTransformedProps(
+      clusters = obj@meta.data[[cluster]],
+      sample = obj@meta.data[[sample]],
+      transform = "logit"
+    )
+    cl_size <- t(as.data.frame.matrix(props$TransformedProps))
+
+    # Create lookup
+    lookup_pre <- data.frame(
+      sample = obj@meta.data[[sample]],
+      patient = obj@meta.data[[patient]],
+      condition = obj@meta.data[[condition]]
+    ) |>
+      distinct()
+
+    lookup_final <- data.frame(sample = rownames(cl_size)) |>
+      left_join(lookup_pre, by = "sample")
+  } else {
+    # Multiple tissues: calculate proportions per tissue, add tissue suffix
+    prop_list <- lapply(object_list, function(obj) {
+      tissue_name <- unique(obj@meta.data[[tissue]])[1]
+
+      # Calculate proportions per sample (which is per patient)
+      props <- speckle::getTransformedProps(
+        clusters = obj@meta.data[[cluster]],
+        sample = obj@meta.data[[sample]],
+        transform = "logit"
+      )
+
+      # Transpose: samples as rows, clusters as columns
+      cl_size_sample <- t(as.data.frame.matrix(props$TransformedProps))
+      
+      # Extract patient ID from sample names
+      rownames(cl_size_sample) <- gsub(
+        pattern = ".*_(P\\d+)$",
+        replacement = "\\1",
+        rownames(cl_size_sample)
+      )
+
+      # Add tissue suffix to cluster names
+      props_df <- as.data.frame(cl_size_sample)
+      names(props_df) <- paste0(names(props_df), "_", tissue_name)
+      props_df$patient <- rownames(cl_size_sample)
+
+      props_df
+    })
+
+    # Merge all tissues by patient
+    cl_size_df <- Reduce(
+      function(x, y) full_join(x, y, by = "patient"),
+      prop_list
+    ) |>
+      as.data.frame()
+    
+    rownames(cl_size_df) <- cl_size_df$patient
+    cl_size <- cl_size_df |> select(-patient) |> as.matrix()
+
+    # Create lookup
+    lookup_df <- do.call(
+      rbind,
+      lapply(object_list, function(obj) {
+        obj@meta.data |> select(all_of(c(patient, condition))) |> distinct()
+      })
+    ) |>
+      distinct() |>
+      as.data.frame()
+    names(lookup_df) <- c("patient", "condition")
+
+    lookup_final <- data.frame(patient = rownames(cl_size)) |>
+      left_join(lookup_df, by = "patient")
+  }
 
   pca_result <- FactoMineR::PCA(
     cl_size,
@@ -78,21 +148,11 @@ pcaSeurat1 <- function(
       pca_result,
       col.var = "contrib",
       gradient.cols = viridis::viridis(100),
-      repel = TRUE
+      repel = TRUE,
+      select.var = list(contrib = 20)
     ) +
     labs(title = "") +
     theme_classic()
-
-  lookup_pre <-
-    data.frame(
-      cluster = object@meta.data[[sample]],
-      condition = object@meta.data[[condition]]
-    ) |>
-    distinct()
-
-  lookup <-
-    data.frame(cluster = rownames(cl_size)) |>
-    left_join(lookup_pre)
 
   factoextra::fviz_pca_ind(pca_result)
 
@@ -128,7 +188,7 @@ pcaSeurat1 <- function(
       pointsize = 5,
       pointshape = 21,
       geom.ind = "point",
-      fill.ind = lookup$condition,
+      fill.ind = lookup_final$condition,
       col.ind = "black",
       palette = "Set2",
       addEllipses = TRUE,
@@ -162,7 +222,7 @@ pcaSeurat1 <- function(
   ggsave(
     file.path(
       dir_output,
-      paste0(object_parse, "_", condition, "_", cluster, ".pdf")
+      paste0(object_name, "_", condition, "_", cluster, ".pdf")
     ),
     width = width,
     height = height,
@@ -171,27 +231,44 @@ pcaSeurat1 <- function(
   return(pca_plots)
 }
 
+# Run PCA on combined CSF and PBMC data
+
+# PCA for CSF only
 pcaSeurat1(
-  object = sc_merge_csf_main,
+  object_list = sc_merge_csf_main,
   cluster = "cluster",
   sample = "sample",
+  patient = "patient",
   condition = "diagnosis",
+  object_name = "csf",
   width = 20,
   height = 5,
   dir_output = file.path("results", "abundance")
 )
 
+# PCA for PBMC only
 pcaSeurat1(
-  object = sc_merge_pbmc_main,
+  object_list = sc_merge_pbmc_main,
   cluster = "cluster",
   sample = "sample",
+  patient = "patient",
   condition = "diagnosis",
+  object_name = "pbmc",
   width = 20,
   height = 5,
   dir_output = file.path("results", "abundance")
 )
 
-lookup |>
-  select(patient, diagnosis) |>
-  print(n = Inf)
-
+# PCA for combined CSF and PBMC
+pcaSeurat1(
+  object_list = list(sc_merge_csf_main, sc_merge_pbmc_main),
+  cluster = "cluster",
+  sample = "sample",
+  patient = "patient",
+  condition = "diagnosis",
+  tissue = "tissue",
+  object_name = "combined_csf_pbmc",
+  width = 20,
+  height = 5,
+  dir_output = file.path("results", "abundance")
+)
